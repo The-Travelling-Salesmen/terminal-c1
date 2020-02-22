7739e901ccc88f46e537fb5aae70a4e7a3703a6e
ankile <lars.ankile@gmail.com>
2020-02-22 15:44:11 -0500

    Add v3-2 with reactive defence

---
+++ snorkeldink-v3-2/adaptive_opening.py
@@ -0,0 +1,68 @@
+import random
+from operator import itemgetter
+
+""" Adaptive opening defence. 
+    Assesses the enemy's defence and makes an opening, so that our EMPs attack weaker side."""
+def build_defences_with_adaptive_opening(game_state, units, is_right_opening, filter_locs):
+    
+    # Place destructors that attack enemy units
+    destructor_locations = [[2, 13], [3, 13], [10, 13], [17, 13], [24, 13], [25, 13]]
+    game_state.attempt_spawn(units.DESTRUCTOR, destructor_locations)
+    
+    if game_state.turn_number < 4:
+        return [], True
+
+    # Find the weaker side of enemy's defence
+    # Open up our filter defence towards that side
+    
+    final_filter_locs = list(filter_locs)
+
+    if game_state.turn_number % 4 == 0:
+        is_right_opening = should_right_be_open(game_state, units) 
+    
+
+    if is_right_opening:
+        remove_filter_at = [[23, 13]]
+        final_filter_locs.append([4, 13])
+
+    else:
+        remove_filter_at = [[4, 13]]
+        final_filter_locs.append([23, 13])
+            
+        
+    game_state.attempt_remove(remove_filter_at)
+
+    final_filter_locs.sort(key=itemgetter(0), reverse=(not is_right_opening))
+
+
+    game_state.attempt_spawn(units.FILTER, final_filter_locs)
+    return final_filter_locs, is_right_opening
+
+
+""" Assess enemy defence & identify weaker side (for opening)
+    Author @RKJ (blame me for the mistakes) """
+def should_right_be_open(game_state, units, weights=None):
+    if not weights:
+        weights = [1, 6] # filter is worth 1 badness pt, destructor - 6 badness pts.
+    
+    weights_by_def_unit = dict(zip([units.FILTER, units.DESTRUCTOR], weights))
+
+    left_strength, right_strength = (0, 0)
+
+    for location in game_state.game_map:
+        if game_state.contains_stationary_unit(location):
+            for unit in game_state.game_map[location]:
+                if unit.player_index == 1 and (unit.unit_type == units.DESTRUCTOR or unit.unit_type == units.FILTER):
+                    if location[0] < 10:
+                        left_strength += weights_by_def_unit[unit.unit_type]
+                    elif location[0] > 17:                    
+                        right_strength += weights_by_def_unit[unit.unit_type]
+    
+    # Return side with less strength
+    if left_strength > right_strength:
+        right = True
+    elif left_strength < right_strength:
+        right = False
+    else:
+        right = bool(random.randint(0, 1))
+    return right
---
+++ snorkeldink-v3-2/defence.py
@@ -0,0 +1,34 @@
+
+
+# NOTE: coords below needs to be adaptive if we add adaptive change left hole vs right
+# TODO: upgrade!
+def build_defences(game_state, units, is_right_opening, filter_locs):
+
+    # Encryptors
+    encryptor_locations = [[13, 10], [14, 10]]
+    game_state.attempt_spawn(units.ENCRYPTOR, encryptor_locations)
+    
+    # Upgrade encryptors
+    game_state.attempt_upgrade(encryptor_locations)
+
+    # More destructors around hole/opening
+    destructor_locations = [[25, 12], [24, 11], [24, 10]] if is_right_opening else [[2, 12], [3, 11], [3, 10]]
+    game_state.attempt_spawn(units.DESTRUCTOR, destructor_locations)
+
+    # One more encryptor
+    # encryptor_locations = [[24, 10]]
+    # game_state.attempt_spawn(units.ENCRYPTOR, encryptor_locations)
+
+    # Upgrade filter wall if additional destructors are added
+    # TODO: upgrade only some of the filters
+    if all(map(game_state.contains_stationary_unit, destructor_locations)):
+        game_state.attempt_upgrade(filter_locs)
+
+    # Center Destructors
+    destructor_locations = [[17, 11], [6, 8], [10, 11], [15, 9], [12, 9], [15, 6], [12, 6]]
+    game_state.attempt_spawn(units.DESTRUCTOR, destructor_locations)
+    
+
+    # Encryptors
+    # encryptor_locations = [[23, 10]]
+    # game_state.attempt_spawn(units.ENCRYPTOR, encryptor_locations)
