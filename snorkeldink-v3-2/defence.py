c374bb6f6390793bbd3d4ee0c7d4bd837800901e
ankile <lars.ankile@gmail.com>
2020-02-27 22:25:56 -0500

    Add some speculative features

--- snorkeldink-v3-2/defence.py
+++ snorkeldink-v3-2/defence.py
@@ -1,11 +1,9 @@
 
 
-# NOTE: coords below needs to be adaptive if we add adaptive change left hole vs right
-# TODO: upgrade!
 def build_defences(game_state, units, is_right_opening, filter_locs):
 
     # Encryptors
-    encryptor_locations = [[13, 10], [14, 10]]
+    encryptor_locations = [[10, 10], [17, 10]]
     game_state.attempt_spawn(units.ENCRYPTOR, encryptor_locations)
     
     # Upgrade encryptors
@@ -15,9 +13,10 @@ def build_defences(game_state, units, is_right_opening, filter_locs):
     destructor_locations = [[25, 12], [24, 11], [24, 10]] if is_right_opening else [[2, 12], [3, 11], [3, 10]]
     game_state.attempt_spawn(units.DESTRUCTOR, destructor_locations)
 
-    # One more encryptor
-    # encryptor_locations = [[24, 10]]
-    # game_state.attempt_spawn(units.ENCRYPTOR, encryptor_locations)
+    # Two more encryptors
+    encryptor_locations = [[10, 8], [17, 8]]
+    game_state.attempt_spawn(units.ENCRYPTOR, encryptor_locations)
+    game_state.attempt_upgrade(encryptor_locations)
 
     # Upgrade filter wall if additional destructors are added
     # TODO: upgrade only some of the filters
@@ -28,7 +27,5 @@ def build_defences(game_state, units, is_right_opening, filter_locs):
     destructor_locations = [[17, 11], [6, 8], [10, 11], [15, 9], [12, 9], [15, 6], [12, 6]]
     game_state.attempt_spawn(units.DESTRUCTOR, destructor_locations)
     
-
-    # Encryptors
-    # encryptor_locations = [[23, 10]]
-    # game_state.attempt_spawn(units.ENCRYPTOR, encryptor_locations)
+    # Upgrade destructors in the back
+    game_state.attempt_upgrade([[3,10], [24,10]])
--- snorkeldink-v3-3/algo_strategy.py
+++ snorkeldink-v3-3/algo_strategy.py
@@ -97,82 +97,6 @@ class AlgoStrategy(gamelib.AlgoCore):
                 emp_location = [[23, 9]]
             game_state.attempt_spawn(EMP, emp_location, 1000)
 
-
-    def build_reactive_defense(self, game_state):
-        """
-        This function builds reactive defenses based on where the enemy scored on us from.
-        We can track where the opponent scored by looking at events in action frames 
-        as shown in the on_action_frame function
-        """
-        for location in self.scored_on_locations:
-            # Build destructor one space above so that it doesn't block our own edge spawn locations
-            build_location = [location[0], location[1]+1]
-            game_state.attempt_spawn(DESTRUCTOR, build_location)
-
-    def stall_with_scramblers(self, game_state):
-        """
-        Send out Scramblers at random locations to defend our base from enemy moving units.
-        """
-        # We can spawn moving units on our edges so a list of all our edge locations
-        friendly_edges = game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_LEFT) + game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
-        
-        # Remove locations that are blocked by our own firewalls 
-        # since we can't deploy units there.
-        deploy_locations = self.filter_blocked_locations(friendly_edges, game_state)
-        
-        # While we have remaining bits to spend lets send out scramblers randomly.
-        while game_state.get_resource(BITS) >= game_state.type_cost(SCRAMBLER)[BITS] and len(deploy_locations) > 0:
-            # Choose a random deploy location.
-            deploy_index = random.randint(0, len(deploy_locations) - 1)
-            deploy_location = deploy_locations[deploy_index]
-            
-            game_state.attempt_spawn(SCRAMBLER, deploy_location)
-            """
-            We don't have to remove the location since multiple information 
-            units can occupy the same space.
-            """
-
-    def emp_line_strategy(self, game_state):
-        """
-        Build a line of the cheapest stationary unit so our EMP's can attack from long range.
-        """
-        # First let's figure out the cheapest unit
-        # We could just check the game rules, but this demonstrates how to use the GameUnit class
-        stationary_units = [FILTER, DESTRUCTOR, ENCRYPTOR]
-        cheapest_unit = FILTER
-        for unit in stationary_units:
-            unit_class = gamelib.GameUnit(unit, game_state.config)
-            if unit_class.cost[game_state.BITS] < gamelib.GameUnit(cheapest_unit, game_state.config).cost[game_state.BITS]:
-                cheapest_unit = unit
-
-        # Now let's build out a line of stationary units. This will prevent our EMPs from running into the enemy base.
-        # Instead they will stay at the perfect distance to attack the front two rows of the enemy base.
-        for x in range(27, 5, -1):
-            game_state.attempt_spawn(cheapest_unit, [x, 11])
-
-        # Now spawn EMPs next to the line
-        # By asking attempt_spawn to spawn 1000 units, it will essentially spawn as many as we have resources for
-        game_state.attempt_spawn(EMP, [24, 10], 1000)
-
-    def least_damage_spawn_location(self, game_state, location_options):
-        """
-        This function will help us guess which location is the safest to spawn moving units from.
-        It gets the path the unit will take then checks locations on that path to 
-        estimate the path's damage risk.
-        """
-        damages = []
-        # Get the damage estimate each path will take
-        for location in location_options:
-            path = game_state.find_path_to_edge(location)
-            damage = 0
-            for path_location in path:
-                # Get number of enemy destructors that can attack the final location and multiply by destructor damage
-                damage += len(game_state.get_attackers(path_location, 0)) * gamelib.GameUnit(DESTRUCTOR, game_state.config).damage_i
-            damages.append(damage)
-        
-        # Now just return the location that takes the least damage
-        return location_options[damages.index(min(damages))]
-
     def detect_enemy_unit(self, game_state, unit_type=None, valid_x = None, valid_y = None):
         total_units = 0
         for location in game_state.game_map:
@@ -181,13 +105,6 @@ class AlgoStrategy(gamelib.AlgoCore):
                     if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
                         total_units += 1
         return total_units
-        
-    def filter_blocked_locations(self, locations, game_state):
-        filtered = []
-        for location in locations:
-            if not game_state.contains_stationary_unit(location):
-                filtered.append(location)
-        return filtered
 
     def on_action_frame(self, turn_string):
         """
